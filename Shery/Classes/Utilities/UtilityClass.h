220ad25cd14a4296c281ebdeb5142f8971a58eb9
Kasper Pihl Tornøe <kasper@pihl.it>
2013-04-20 07:38:12 +0200

    UAModalPanel + Cleanup

--- Shery/Classes/Utilities/UtilityClass.h
+++ Shery/Classes/Utilities/UtilityClass.h
@@ -5,39 +5,16 @@
 //  Created by Kasper Pihl Tornøe on 09/03/13.
 //  Copyright (c) 2013 Pihl IT. All rights reserved.
 //
-#import "LocationClass.h"
 #import <Foundation/Foundation.h>
+
 #define UTILITY [UtilityClass instance]
-#define DARK_BLUE_COLOR [UtilityClass colorWithRed:0 green:8 blue:98 alpha:1]
-#define PINK_COLOR [UtilityClass colorWithRed:255 green:55 blue:140 alpha:1]
-#define LIGHT_BLUE_COLOR [UtilityClass colorWithRed:66 green:151 blue:255 alpha: 1]
-@class GameModel;
-
-
 
 @interface UtilityClass : NSObject
--(void)sendError:(NSError *)error message:(NSString *)message type:(NSString *)type screenshot:(BOOL)screenshot;
 -(int)ageForBirthday:(NSString *)birthday;
-+(UIButton*)buttonWithTitle:(NSString*)title boy:(BOOL)boy width:(CGFloat)width;
 +(UtilityClass*)instance;
 -(NSNumber*)versionNumber;
 -(void)confirmBoxWithTitle:(NSString*)title andMessage:(NSString*)message block:(SuccessfulBlock)block;
 -(void)popupBoxWithTitle:(NSString*)title andMessage:(NSString*)message buttons:(NSArray*)buttons block:(SuccessfulBlock)block;
 +(NSString*)generateIdWithLength:(NSInteger)length;
--(BOOL)settingIsSet:(NSString *)setting;
--(void)setSetting:(NSString *)setting;
--(NSDate *)dateForKey:(NSString *)setting;
--(void)setDate:(NSDate*)date forKey:(NSString *)setting;
--(void)setDictionary:(NSDictionary *)dictionary forSetting:(NSString *)setting;
--(NSDictionary*)dictionaryForSetting:(NSString *)setting;
--(NSInteger)intForSetting:(NSString *)setting;
--(void)setInt:(NSInteger)integer forSetting:(NSString *)setting;
--(NSString *)stringForSetting:(NSString *)setting;
--(void)setString:(NSString*)string forSetting:(NSString *)setting;
 +(UIColor*)colorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
-
-+(NSString *)readableTimeLeft:(NSInteger)timeLeft;
--(NSString *)displayBirthday:(NSString*)birthdayString;
-//+(NSString *)readableChatFromTime:(NSDate *)time;
-+(UIButton*)facebookButtonWithAmount:(NSInteger)amount;
 @end
--- Shery/Classes/Utilities/UtilityClass.m
+++ Shery/Classes/Utilities/UtilityClass.m
@@ -8,7 +8,6 @@
 
 #import "UtilityClass.h"
 #import "AppDelegate.h"
-#import "ImageClass.h"
 #import "KPParseCommunicator.h"
 @interface UtilityClass () <UIAlertViewDelegate>
 @property (nonatomic,strong) NSUserDefaults *userDefaults;
@@ -28,44 +27,10 @@ static UtilityClass *sharedObject;
     }
     return _queue;
 }
--(void)sendError:(NSError *)error message:(NSString *)message type:(NSString *)type screenshot:(BOOL)screenshot{
-    /*dispatch_async(self.queue, ^{
-        NSError *error;
-        PFObject *errorObject = [PFObject objectWithClassName:@"Error"];
-        if(message) [errorObject setObject:message forKey:@"message"];
-        if([error description]) [errorObject setObject:[error description] forKey:@"error"];
-        if(type) [errorObject setObject:type forKey:@"type"];
-        if(screenshot){
-            UIImage *screen = [ImageClass screenshot];
-            NSData *screenData = UIImagePNGRepresentation(screen);
-            PFFile *pictureFile = [PFFile fileWithData:screenData];
-            [pictureFile save:&error];
-            if(!error) [errorObject setObject:pictureFile forKey:@"screenshot"];
-        }
-        [errorObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-            if(!succeeded) [errorObject saveEventually];
-        }];
-    });*/
- 
-}
+
 +(UtilityClass*)instance{
     if(sharedObject == nil) sharedObject = [[super allocWithZone:NULL] init];
     return sharedObject;
-}
-+(UIButton *)buttonWithTitle:(NSString *)title boy:(BOOL)boy width:(CGFloat)width{
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, width, 50)];
-    UIColor *textColor;
-    if(boy) textColor = DARK_BLUE_COLOR;
-    else textColor = PINK_COLOR;
-    [button setTitleColor:textColor forState:UIControlStateNormal];
-    
-    button.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
-    NSString *realTitle = [NSString stringWithFormat:@"%@",title];//">   "
-    [button setTitle:realTitle forState:UIControlStateNormal];
-    UIImage *backgroundButtonImage = [[UIImage imageNamed:@"big_button_background"] resizableImageWithCapInsets:UIEdgeInsetsMake(7, 7, 7, 7)];
-    
-    [button setBackgroundImage:backgroundButtonImage forState:UIControlStateNormal];
-    return button;
 }
 -(NSNumber *)versionNumber{
     NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
@@ -100,34 +65,7 @@ static UtilityClass *sharedObject;
     }
     self.block = nil;
 }
-+(NSString *)readableTimeLeft:(NSInteger)timeLeft{
-    NSString *timeString;
-   
-    NSInteger minutes = timeLeft/60;
-    if(minutes < 5) timeString = @"<5m";
-    else if(minutes <= 60) timeString = [NSString stringWithFormat:@"%im",minutes];
-    else{
-        NSInteger hours = timeLeft/3600;
-        
-        timeString = [NSString stringWithFormat:@"%ih",hours];
-    }
-    return timeString;
-}
-/*+(NSString *)readableChatFromTime:(NSDate *)time{
-    NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
-    if([time isToday]){
-        [outputFormatter setDateFormat:@"HH:mm"];
-    }
-    else if([time isYesterday]){
-        return @"Yesterday";
-    }
-    else{
-        [outputFormatter setDateFormat:@"dd/MM/yyyy"];
-    }
-    
-    
-    return [outputFormatter stringFromDate:time];
-}*/
+
 +(UIColor*)colorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha{
     return [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
 }
@@ -147,65 +85,7 @@ static UtilityClass *sharedObject;
     if(!_userDefaults) _userDefaults = [NSUserDefaults standardUserDefaults];
     return _userDefaults;
 }
--(NSDate *)dateForKey:(NSString *)setting{
-    return (NSDate*)[self.userDefaults objectForKey:setting];
-}
--(void)setDate:(NSDate*)date forKey:(NSString *)setting{
-    [self.userDefaults setObject:date forKey:setting];
-}
--(NSInteger)intForSetting:(NSString *)setting{
-    return [self.userDefaults integerForKey:setting];
-}
--(void)setInt:(NSInteger)integer forSetting:(NSString *)setting{
-    [self.userDefaults setInteger:integer forKey:setting];
-}
--(NSString *)stringForSetting:(NSString *)setting{
-    return [self.userDefaults stringForKey:setting];
-}
--(void)setString:(NSString*)string forSetting:(NSString *)setting{
-    [self.userDefaults setObject:string forKey:setting];
-}
--(BOOL)settingIsSet:(NSString *)setting{
-    return [self.userDefaults boolForKey:setting];
-}
--(void)setDictionary:(NSDictionary *)dictionary forSetting:(NSString *)setting{
-    [self.userDefaults setObject:dictionary forKey:setting];
-}
--(NSDictionary*)dictionaryForSetting:(NSString *)setting{
-    return [self.userDefaults dictionaryForKey:setting];
-}
--(void)setSetting:(NSString *)setting{
-    [self.userDefaults setBool:YES forKey:setting];
-}
--(void)unsetSetting:(NSString *)setting{
-    [self.userDefaults setBool:NO forKey:setting];
-}
--(NSString *)displayBirthday:(NSString*)birthdayString{
-    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-    [dateFormat setDateFormat:@"MM/dd/yyyy"];
-    NSDate *date = [dateFormat dateFromString:birthdayString];
-    
-    NSDateFormatter *outputFormat = [[NSDateFormatter alloc] init];
-    [outputFormat setDateFormat:@"dd/MM/yyyy"];
-    return [outputFormat stringFromDate:date];
-    
-}
-+(UIButton*)facebookButtonWithAmount:(NSInteger)amount{
-    CGFloat leftInset = 45;
-    CGFloat rightInset = 8;
-    NSString *titleText;
-    if(amount > 0) titleText = [NSString stringWithFormat:@"Share     +%ip",amount];
-    else titleText = @"Share";
-    UIButton *facebookButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 42)];
-    [facebookButton setBackgroundImage:[[UIImage imageNamed:@"facebook_button_background"] resizableImageWithCapInsets:UIEdgeInsetsMake(5, 40, 5, 5)] forState:UIControlStateNormal];
-    [facebookButton setTitle:titleText forState:UIControlStateNormal];
-    facebookButton.titleEdgeInsets = UIEdgeInsetsMake(0, leftInset, 0, rightInset);
-    [facebookButton sizeToFit];
-    facebookButton.frame = CGRectMake(0, 0, facebookButton.frame.size.width+leftInset+rightInset, 42);
-    return facebookButton;
-}
 -(int)ageForBirthday:(NSString *)birthday{
-    
     // Convert string to date object
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"MM/dd/yyyy"];
