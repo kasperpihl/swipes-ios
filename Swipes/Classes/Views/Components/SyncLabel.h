307ed08ed1d246a7665cd319b11e080c61695b3e
Kasper Pihl Tornøe <kasper@pihl.it>
2014-05-27 11:57:12 +0300

    Updated icon and font

---
+++ Swipes/Classes/Views/Components/SyncLabel.h
@@ -0,0 +1,13 @@
+//
+//  SyncLabel.h
+//  Swipes
+//
+//  Created by Kasper Pihl Tornøe on 27/05/14.
+//  Copyright (c) 2014 Pihl IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyncLabel : UIButton
+
+@end
---
+++ Swipes/Classes/Views/Components/SyncLabel.m
@@ -0,0 +1,31 @@
+//
+//  SyncLabel.m
+//  Swipes
+//
+//  Created by Kasper Pihl Tornøe on 27/05/14.
+//  Copyright (c) 2014 Pihl IT. All rights reserved.
+//
+
+#import "SyncLabel.h"
+
+@implementation SyncLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
