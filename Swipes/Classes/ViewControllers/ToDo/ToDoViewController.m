bcc4926fa8a38143b1c9f185196ad70c427840e3
Kasper Pihl Tornøe <kasper@pihl.it>
2014-07-19 10:54:02 +0200

    Prevent title override while editing

--- Swipes/Classes/ViewControllers/ToDo/ToDoViewController.m
+++ Swipes/Classes/ViewControllers/ToDo/ToDoViewController.m
@@ -400,7 +400,8 @@ typedef NS_ENUM(NSUInteger, KPEditMode){
     // Save objectId - if deleted from sync we know it here
     if (self.model.objectId)
         self.objectId = self.model.objectId;
-    self.textView.text = self.model.title;
+    if( self.activeEditMode != KPEditModeTitle )
+        self.textView.text = self.model.title;
     self.cellType = [self.model cellTypeForTodo];
     [self updateTags];
     [self updateSchedule];
