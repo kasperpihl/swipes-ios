b676df1c0b9ab0617598bb3a5ad55e0207c40b41
Kasper Pihl Tornøe <kasper@pihl.it>
2014-11-29 13:08:58 +0200

    Initial commit on the HelpTopMenu

---
+++ Swipes/Classes/Views/TopMenus/HelpTopMenu.h
@@ -0,0 +1,13 @@
+//
+//  HelpTopMenu.h
+//  Swipes
+//
+//  Created by Kasper Pihl Tornøe on 29/11/14.
+//  Copyright (c) 2014 Pihl IT. All rights reserved.
+//
+
+#import "TopMenu.h"
+
+@interface HelpTopMenu : TopMenu
+
+@end
---
+++ Swipes/Classes/Views/TopMenus/HelpTopMenu.m
@@ -0,0 +1,21 @@
+//
+//  HelpTopMenu.m
+//  Swipes
+//
+//  Created by Kasper Pihl Tornøe on 29/11/14.
+//  Copyright (c) 2014 Pihl IT. All rights reserved.
+//
+
+#import "HelpTopMenu.h"
+
+@implementation HelpTopMenu
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
