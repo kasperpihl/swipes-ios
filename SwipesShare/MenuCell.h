17965a79331af7b80bb3dddf87c6e4bb211b24e6
Stanimir Karoserov <demosten@gmail.com>
2015-04-20 17:06:50 +0300

    updated Share Extension design

---
+++ SwipesShare/MenuCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuCell.h
+//  Swipes
+//
+//  Created by demosten on 4/20/15.
+//  Copyright (c) 2015 Pihl IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel* mainLabel;
+
+@end
