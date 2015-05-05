63ae2cdfa635886ccebdc9fd72b71d35e2dc0740
Stanimir Karoserov <demosten@gmail.com>
2015-05-05 12:58:24 +0300

    added pull to synchronize

---
+++ Swipes/Classes/Views/PullToRefresh/PullToRefreshView.h
@@ -0,0 +1,13 @@
+//
+//  PullToRefreshView.h
+//  Swipes
+//
+//  Created by demosten on 5/4/15.
+//  Copyright (c) 2015 Pihl IT. All rights reserved.
+//
+
+#import "BPRRefreshView.h"
+
+@interface PullToRefreshView : BPRRefreshView
+
+@end
